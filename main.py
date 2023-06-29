b423c70e392744ae7f399897749e30d2215b5c45
janandrzejewski <janandrzejewski@aol.com>
2023-06-29 21:57:06 +0200

    Refactored code and added regex pattern matching to extract data from NASA's response

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import configparser
 from datetime import datetime, timedelta
 import requests
+import re
+
 
 class DataObject:
     def __init__(self, id, begin, end):
@@ -15,16 +17,16 @@ class DataObject:
 
 def read_config():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
-    email = config.get('Credentials', 'email')
-    password = config.get('Credentials', 'password')
-    page_name = config.get('Page', 'name')
+    email = config.get("Credentials", "email")
+    password = config.get("Credentials", "password")
+    page_name = config.get("Page", "name")
 
     return email, password, page_name
 
 
-def log_in(email, password,page_name):
+def log_in(email, password, page_name):
     driver = webdriver.Firefox()
     driver.get(page_name)
     element = driver.find_element(By.NAME, "email")
@@ -37,18 +39,14 @@ def log_in(email, password,page_name):
     driver.find_element(
         By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/ul[2]/li[3]/a/span"
     ).click()
-    driver.find_element(
-        By.ID, "sites"
-    ).click()
+    driver.find_element(By.ID, "sites").click()
     driver.find_element(
         By.XPATH, "/html/body/div[3]/div/div/div[2]/table/tbody/tr[4]/td[1]/a/i"
     ).click()
     driver.find_element(
         By.XPATH, "/html/body/div[3]/div/div/div[2]/table/tbody/tr[1]/td[1]/a/i"
     ).click()
-    driver.find_element(
-        By.XPATH, "/html/body/div[3]/div/div/div[3]/button"
-    ).click()
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[3]/button").click()
 
     return driver
 
@@ -65,7 +63,7 @@ def get_info(driver):
         cells = row.find_all("td")
 
         if len(cells) >= 8:
-            id = cells[1].find('span', id='id')
+            id = cells[1].find("span", id="id")
             if id:
                 name_text = id.text
 
@@ -81,23 +79,36 @@ def get_info(driver):
 def close_website(driver):
     driver.quit()
 
+
 def get_position(data_objects):
     for object in data_objects:
-        url = 'https://ssd.jpl.nasa.gov/api/horizons.api'
-        start_time = datetime.today().strftime('%Y-%m-%d') +' ' + object.begin
+        url = "https://ssd.jpl.nasa.gov/api/horizons.api"
+        start_time = datetime.today().strftime("%Y-%m-%d") + " " + object.begin
         if object.begin > object.end:
-            stop_time = (datetime.today()+timedelta(days=1)).strftime('%Y-%m-%d') +' '+ object.end
+            stop_time = (
+                (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+                + " "
+                + object.end
+            )
         else:
-            stop_time = datetime.today().strftime('%Y-%m-%d') +' '+ object.end
-        url += "?format=text&COMMAND='{}'&OBJ_DATA=NO&EPHEM_TYPE=OBSERVER".format(object.id)
-        url += "&START_TIME='{}'&STOP_TIME='{}'&STEP_SIZE='1h'&QUANTITIES=1".format(start_time,stop_time)
-        print(start_time, stop_time)
+            stop_time = datetime.today().strftime("%Y-%m-%d") + " " + object.end
+        url += "?format=text&COMMAND='{}'&OBJ_DATA=NO&EPHEM_TYPE=OBSERVER".format(
+            object.id
+        )
+        url += "&START_TIME='{}'&STOP_TIME='{}'&STEP_SIZE='1m'&QUANTITIES='1'".format(
+            start_time, stop_time
+        )
         response = requests.get(url)
-        print(response.text)
+        match = re.search(r"\$\$SOE.*?\$\$EOE", response.text, re.DOTALL)
+        if match:
+            fragment = match.group()
+            print(object.id)
+            print(fragment)
+
 
 def main():
     email, password, page_name = read_config()
-    driver = log_in(email, password,page_name)
+    driver = log_in(email, password, page_name)
     data_objects = get_info(driver)
     close_website(driver)
     get_position(data_objects)
